6f7850b5fd4a17f91f3e1867df2d1b77fb4dec1a
‘name <18037297220@163.com>
2020-06-09 14:50:21 +0800

    更换数据库连接

--- config/database.php
+++ config/database.php
@@ -42,17 +42,17 @@ return [
 
         'mysql' => [
             'read' => [
-                'host' => ['114.67.97.70'],
+                'host' => ['129.211.114.135'],
             ],
             'write' => [
-                'host' => ['114.67.97.70'],
+                'host' => ['129.211.114.135'],
             ],
 
             'sticky'    => false,
             'driver'    => 'mysql',
             'database'  => 'huanqiu',
             'username'  => 'huanqiu',
-            'password'  => 'huanqiu',
+            'password'  => 'lcf2954626',
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => 'hq_',
