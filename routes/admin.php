bdd00bd22dd92c4396432efc837dc00bbe0d29b7
‘name <18037297220@163.com>
2020-06-10 10:28:06 +0800

    新增会员充值查询

--- routes/admin.php
+++ routes/admin.php
@@ -62,6 +62,7 @@ Route::group(['namespace' => "Admin",'middleware' => ['auth', 'permission']], fu
     Route::get('/agent/subUser/{id}','AgentListController@user');//下级会员
     Route::resource('/userDay',     'UserDayEndController');//会员日结表
     Route::resource('/daw','DepositAndWithController');//会员提现查询
+    Route::resource('/cz','CzController');//会员充值查询
     Route::resource('/deluser','DelUserController');//已删会员
     Route::resource('/delagent','DelAgentController');//已删代理
     Route::resource('/down','DownController');//下分请求
