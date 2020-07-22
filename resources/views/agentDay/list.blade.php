3e3a55a33ae925371ce1a50dc8c5f1077ac62c53
‘name <18037297220@163.com>
2020-07-22 09:34:41 +0800

    调整本周，上周日期校准

--- resources/views/agentDay/list.blade.php
+++ resources/views/agentDay/list.blade.php
@@ -124,7 +124,7 @@
                 var nowDay = now.getDate();//当前日
                 var nowMonth = now.getMonth();//当前月
                 var nowYear = now.getFullYear();//当前年
-                var weekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek);
+                var weekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek + 1);
                 $("input[name='begin']").val(formatDate(weekStartDate))
                 $("input[name='end']").val(formatDate(now))
             });
@@ -143,8 +143,8 @@
                 var nowDay = now.getDate();            //当前日
                 var nowMonth = now.getMonth();         //当前月
                 var nowYear = now.getFullYear();           //当前年
-                var getUpWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek -7);
-                var getUpWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek - 7));
+                var getUpWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek -7 + 1);
+                var getUpWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek - 7) + 1);
                 $("input[name='begin']").val(formatDate(getUpWeekStartDate))
                 $("input[name='end']").val(formatDate(getUpWeekEndDate))
             });
--- resources/views/onAgent/agentDay/list.blade.php
+++ resources/views/onAgent/agentDay/list.blade.php
@@ -74,9 +74,9 @@
                 <td class="hidden-xs">{{$info['pump']}}%</td>
                 <td class="hidden-xs">{{number_format($info['pumpMoney']/100,2)}}</td>
                 <td class="hidden-xs">{{$info['proportion']}}%</td>
-                <td class="hidden-xs">0.00</td>
+                <td class="hidden-xs">{{number_format($info['kesun']/100,2)}}</td>
                 <td class="hidden-xs">
-
+                    {{number_format($info['pumpMoney']/100 + $info['kesun']/100,2)}}
                 </td>
                 <td class="hidden-xs">
                     <div class="layui-inline">
@@ -116,7 +116,7 @@
                 var nowDay = now.getDate();//当前日
                 var nowMonth = now.getMonth();//当前月
                 var nowYear = now.getFullYear();//当前年
-                var weekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek);
+                var weekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek +1);
                 $("input[name='begin']").val(formatDate(weekStartDate))
                 $("input[name='end']").val(formatDate(now))
             });
@@ -135,8 +135,8 @@
                 var nowDay = now.getDate();            //当前日
                 var nowMonth = now.getMonth();         //当前月
                 var nowYear = now.getFullYear();           //当前年
-                var getUpWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek -7);
-                var getUpWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek - 7));
+                var getUpWeekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek -7 + 1);
+                var getUpWeekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek - 7) + 1);
                 $("input[name='begin']").val(formatDate(getUpWeekStartDate))
                 $("input[name='end']").val(formatDate(getUpWeekEndDate))
             });
