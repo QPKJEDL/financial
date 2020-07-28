8c6b0c581d7a3b59d15c62c9a697c6c12bd05dc2
‘name <18037297220@163.com>
2020-07-28 14:36:38 +0800

    修复代码报错

--- resources/views/desk/list.blade.php
+++ resources/views/desk/list.blade.php
@@ -54,7 +54,7 @@
                 <td class="hidden-xs">{{$info['time']}}</td>
                 <td class="hidden-xs">{{$info['game_id']}}</td>
                 <td class="hidden-xs">{{$info['desk_name']}}</td>
-                <td class="hidden-xs">{{$input['boot_num']}}</td>
+                <td class="hidden-xs">{{$input['boot_num'] or 0}}</td>
                 <td class="hidden-xs">{{number_format($info['betMoney']/100,2)}}</td>
                 <td class="hidden-xs">{{number_format($info['winAndErr']/100,2)}}</td>
                 <td class="hidden-xs">{{number_format(($info['money']/100) * 0.009,2)}}</td>
