14a8a62c95534028ca05541887c26b0f61fd6fdc
‘name <18037297220@163.com>
2020-08-22 14:47:34 +0800

    1

--- app/Http/Controllers/Admin/CzController.php
+++ app/Http/Controllers/Admin/CzController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Czrecord;
+use App\Models\HqUser;
 use Illuminate\Http\Request;
 
 /**
@@ -36,11 +37,60 @@ class CzController extends Controller
         if (true==$request->has('account')){
             $sql->where('user.account','=',$request->input('account'));
         }
+        if (true==$request->has('limit'))
+        {
+            $limit = $request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
         $sql->orderBy('czrecord.creatime','desc');
-        $data = $sql->paginate(10)->appends($request->all());
+        $data = $sql->paginate($limit)->appends($request->all());
         foreach ($data as $key=>$value){
             $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
         }
-        return view('czrecord.list',['list'=>$data,'input'=>$request->all(),'min'=>config('admin.min_date')]);
+        return view('czrecord.list',['list'=>$data,'input'=>$request->all(),'limit'=>$limit]);
+    }
+
+    /**
+     * 获取会员的充值记录
+     * @param $id
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+     */
+    public function getCzRecordList($id,Request $request)
+    {
+        $userInfo = $id?HqUser::find($id):[];
+        $request->offsetSet('account',$userInfo['account']);
+        $map = array();
+        $sql = Czrecord::query();
+        $sql->leftJoin('user','user.user_id','=','czrecord.user_id')
+            ->leftJoin('agent_users','agent_users.id','=','user.agent_id')
+            ->select('czrecord.creatime','czrecord.pay_type','user.nickname','user.account','agent_users.username','agent_users.nickname as agentName','czrecord.score')
+            ->where($map);
+        if (true==$request->has('begin')){
+            $begin = strtotime($request->input('begin'));
+            if (true==$request->has('end')){
+                $end = strtotime('+1day',strtotime($request->input('end')))-1;
+            }else{
+                $end = strtotime('+1day',time())-1;
+            }
+            $sql->whereBetween('czrecord.creatime',[$begin,$end]);
+        }
+        if (true==$request->has('limit'))
+        {
+            $limit = $request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
+        $sql->orderBy('czrecord.creatime','desc');
+        $data = $sql->paginate($limit)->appends($request->all());
+        foreach ($data as $key=>$value){
+            $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
+        }
+        return view('czrecord.list',['list'=>$data,'input'=>$request->all(),'limit'=>$limit]);
     }
 }
