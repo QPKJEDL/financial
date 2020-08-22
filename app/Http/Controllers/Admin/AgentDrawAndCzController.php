9e07d0698ffab27553d7359aa993ccb5620cccbf
‘name <18037297220@163.com>
2020-08-22 14:45:55 +0800

    1

--- app/Http/Controllers/Admin/AgentDrawAndCzController.php
+++ app/Http/Controllers/Admin/AgentDrawAndCzController.php
@@ -15,7 +15,15 @@ class AgentDrawAndCzController extends Controller
         $map = array();
         if (true==$request->has('account'))
         {
-            $map['agent_users.username']=$request->input('account');
+            $map['agent_users.username']=HttpFilter($request->input('account'));
+        }
+        if (true==$request->has('status'))
+        {
+            $map['agent_billflow.status']=(int)$request->input('status');
+        }
+        if (true==$request->has('userType'))
+        {
+            $map['agent_users.userType']=(int)$request->input('userType');
         }
         $sql = AgentBill::query();
         $sql->leftJoin('agent_users','agent_billflow.agent_id','=','agent_users.id')
@@ -27,11 +35,63 @@ class AgentDrawAndCzController extends Controller
             $end=strtotime('+1day',$start);
             $sql->whereBetween('agent_billflow.creatime',[$start,$end]);
         }
-        $data = $sql->where($map)->orderBy('agent_billflow.creatime','desc')->paginate(10)->appends($request->all());
+        if (true==$request->has('limit'))
+        {
+            $limit = (int)$request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
+        $data = $sql->where($map)->orderBy('agent_billflow.creatime','desc')->paginate($limit)->appends($request->all());
         foreach ($data as $key=>$value)
         {
             $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
         }
-        return view('agentBill.list',['list'=>$data,'input'=>$request->all(),'min'=>config('admin.min_date')]);
+        return view('agentBill.list',['list'=>$data,'input'=>$request->all(),'limit'=>$limit]);
+    }
+
+    /**
+     * 根据代理id查询记录
+     * @param $id
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+     */
+    public function getRecordByAgentId($id,Request $request)
+    {
+        $map = array();
+        $map['agent_billflow.agent_id']=(int)$id;
+        if (true==$request->has('account'))
+        {
+            $map['agent_users.username']=HttpFilter($request->input('account'));
+        }
+        if (true==$request->has('status'))
+        {
+            $map['agent_billflow.status']=(int)$request->input('status');
+        }
+        $sql = AgentBill::query();
+        $sql->leftJoin('agent_users','agent_billflow.agent_id','=','agent_users.id')
+            ->leftJoin('user','user.user_id','=','agent_billflow.user_id')
+            ->select('agent_billflow.*','agent_users.nickname as agentName','agent_users.username','user.nickname as uName','user.account');
+        if (true==$request->has('begin'))
+        {
+            $start=strtotime($request->input('begin'));
+            $end=strtotime('+1day',$start);
+            $sql->whereBetween('agent_billflow.creatime',[$start,$end]);
+        }
+        if (true==$request->has('limit'))
+        {
+            $limit = (int)$request->input('limit');
+        }
+        else
+        {
+            $limit = 10;
+        }
+        $data = $sql->where($map)->orderBy('agent_billflow.creatime','desc')->paginate($limit)->appends($request->all());
+        foreach ($data as $key=>$value)
+        {
+            $data[$key]['creatime']=date('Y-m-d H:i:s',$value['creatime']);
+        }
+        return view('agentBill.list',['list'=>$data,'input'=>$request->all(),'limit'=>$limit]);
     }
 }
