e3d1d72fce4f135451589cb4f4ecd7f1e1ce8a5f
UltrmanOrbr <1216281190@qq.com>
2020-11-30 09:36:15 +0800

    Orb 财务后台 11.30:api路由屏蔽代码

--- routes/api.php
+++ routes/api.php
@@ -19,35 +19,35 @@ use Illuminate\Support\Facades\DB;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/ket',function (Request $request){
-
-    for($i=0;$i<1;$i++){
-        DB::beginTransaction();
-        $log=DB::table('users_money')->where("userid",1)->lockForUpdate()->first();
-        if($log->money>0){
-            echo $log->money."--";
-            DB::table('users_money')->where("userid",1)->decrement('money', 5);
-            DB::commit();
-        }else{
-            echo $log->money;
-            //DB::rollBack();
-        }
-    }
-});
-Route::get('/content',function (Request $request){
-
-    echo file_get_contents("http://114.67.97.70:8032/api/ket");
-    echo "<br>";
-    echo file_get_contents("http://114.67.97.70:8032/api/ket");
-    echo "<br>";
-    echo file_get_contents("http://114.67.97.70:8032/api/ket");
-    echo "<br>";
-    echo file_get_contents("http://114.67.97.70:8032/api/ket");
-    echo "<br>";
-});
-Route::group(['namespace' => "Ucenter"], function () {
-    Route::get('/square/publish',"SquareController@publish");
-    Route::get('/square/index',"SquareController@index");
-    Route::get('/test',"SquareController@test");
-    Route::get('/test2',"SquareController@test2");
-});
+//Route::get('/ket',function (Request $request){
+//
+//    for($i=0;$i<1;$i++){
+//        DB::beginTransaction();
+//        $log=DB::table('users_money')->where("userid",1)->lockForUpdate()->first();
+//        if($log->money>0){
+//            echo $log->money."--";
+//            DB::table('users_money')->where("userid",1)->decrement('money', 5);
+//            DB::commit();
+//        }else{
+//            echo $log->money;
+//            //DB::rollBack();
+//        }
+//    }
+//});
+//Route::get('/content',function (Request $request){
+//
+//    echo file_get_contents("http://114.67.97.70:8032/api/ket");
+//    echo "<br>";
+//    echo file_get_contents("http://114.67.97.70:8032/api/ket");
+//    echo "<br>";
+//    echo file_get_contents("http://114.67.97.70:8032/api/ket");
+//    echo "<br>";
+//    echo file_get_contents("http://114.67.97.70:8032/api/ket");
+//    echo "<br>";
+//});
+//Route::group(['namespace' => "Ucenter"], function () {
+//    Route::get('/square/publish',"SquareController@publish");
+//    Route::get('/square/index',"SquareController@index");
+//    Route::get('/test',"SquareController@test");
+//    Route::get('/test2',"SquareController@test2");
+//});
